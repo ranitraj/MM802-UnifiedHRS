a436c55e848836774b00a3deeeff8628ac0c3b86
Ranit Raj Ganguly <ranitrajganguly@Ranits-Mac-mini.hitronhub.home>
2023-04-05 02:58:26 -0600

    Implementing Firebase onMessageReceived() function

--- app/src/main/java/com/example/unifiedhrs/FirebaseNotificationService.kt
+++ app/src/main/java/com/example/unifiedhrs/FirebaseNotificationService.kt
@@ -1,12 +1,20 @@
 package com.example.unifiedhrs
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class FirebaseNotificationService: FirebaseMessagingService() {
+    private val tag: String? = FirebaseNotificationService::class.simpleName
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+        Log.d(tag, "RRG onNewToken() called with: token = $token")
+    }
 
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        Log.e(tag, "RRG onMessageReceived() called with: message = $message")
     }
 
 }
--- app/src/main/java/com/example/unifiedhrs/MainActivity.kt
+++ app/src/main/java/com/example/unifiedhrs/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.example.unifiedhrs
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.unifiedhrs.databinding.ActivityMainBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     private val tag: String? = MainActivity::class.simpleName
@@ -15,9 +23,26 @@ class MainActivity : AppCompatActivity() {
         val view: View = binding.root
 
         setContentView(view)
+        askNotificationPermission()
 
         initUI()
         initClickListeners()
+        initFirebase()
+    }
+
+    private fun initFirebase() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) { Log.w(tag, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            Log.e(tag, "RRG Token = $token")
+            Toast.makeText(baseContext, "Token obtained", Toast.LENGTH_SHORT).show()
+        })
     }
 
     private fun initUI() {
@@ -29,26 +54,45 @@ class MainActivity : AppCompatActivity() {
     private fun initClickListeners() {
         binding.tvPatientInfo.setOnClickListener {
             showPatientHistoryUI()
-
-            binding.tvPatientInfo.setTextColor(resources.getColor(R.color.lightgrey))
-            binding.tvMedicalHistory.setTextColor(resources.getColor(R.color.unselected))
-            binding.tvAccess.setTextColor(resources.getColor(R.color.unselected))
         }
 
         binding.tvMedicalHistory.setOnClickListener {
             showMedicalHistoryUI()
-
-            binding.tvPatientInfo.setTextColor(resources.getColor(R.color.unselected))
-            binding.tvMedicalHistory.setTextColor(resources.getColor(R.color.lightgrey))
-            binding.tvAccess.setTextColor(resources.getColor(R.color.unselected))
         }
 
         binding.tvAccess.setOnClickListener {
             showAccessUI()
+        }
+    }
 
-            binding.tvMedicalHistory.setTextColor(resources.getColor(R.color.unselected))
-            binding.tvPatientInfo.setTextColor(resources.getColor(R.color.unselected))
-            binding.tvAccess.setTextColor(resources.getColor(R.color.lightgrey))
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            Toast.makeText(
+                this,
+                "Notification Permission Granted!",
+                Toast.LENGTH_LONG
+            ).show()
+        } else {
+            Toast.makeText(
+                this,
+                "Unified HRS will NOT show Notifications!",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else {
+                // Ask for permission
+                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+            }
         }
     }
 
@@ -56,17 +100,29 @@ class MainActivity : AppCompatActivity() {
         binding.layoutPatientInfo.visibility = View.VISIBLE
         binding.layoutMedicalHistory.visibility = View.GONE
         binding.layoutAccess.visibility = View.GONE
+
+        binding.tvPatientInfo.setTextColor(resources.getColor(R.color.lightgrey))
+        binding.tvMedicalHistory.setTextColor(resources.getColor(R.color.unselected))
+        binding.tvAccess.setTextColor(resources.getColor(R.color.unselected))
     }
 
     private fun showMedicalHistoryUI() {
         binding.layoutMedicalHistory.visibility = View.VISIBLE
         binding.layoutPatientInfo.visibility = View.GONE
         binding.layoutAccess.visibility = View.GONE
+
+        binding.tvPatientInfo.setTextColor(resources.getColor(R.color.unselected))
+        binding.tvMedicalHistory.setTextColor(resources.getColor(R.color.lightgrey))
+        binding.tvAccess.setTextColor(resources.getColor(R.color.unselected))
     }
 
     private fun showAccessUI() {
         binding.layoutAccess.visibility = View.VISIBLE
         binding.layoutMedicalHistory.visibility = View.GONE
         binding.layoutPatientInfo.visibility = View.GONE
+
+        binding.tvMedicalHistory.setTextColor(resources.getColor(R.color.unselected))
+        binding.tvPatientInfo.setTextColor(resources.getColor(R.color.unselected))
+        binding.tvAccess.setTextColor(resources.getColor(R.color.lightgrey))
     }
 }
