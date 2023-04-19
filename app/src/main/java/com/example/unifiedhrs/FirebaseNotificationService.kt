335496bee16a49d271b185d2de8df9b1d26dcf52
Ranit Raj Ganguly <ranitrajganguly@Ranits-Mac-mini.hitronhub.home>
2023-04-19 11:54:01 -0600

    Adding In-App Notification feature

--- app/src/main/java/com/example/unifiedhrs/FirebaseNotificationService.kt
+++ app/src/main/java/com/example/unifiedhrs/FirebaseNotificationService.kt
@@ -2,10 +2,19 @@ package com.example.unifiedhrs
 
 import android.R
 import android.app.AlertDialog
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.util.TypedValue
 import android.view.Window
 import android.view.WindowManager
+import androidx.core.app.NotificationCompat
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -25,54 +34,119 @@ class FirebaseNotificationService: FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
         Log.e(tag, "RRG onMessageReceived() called with: message = $message")
-        showInAppPermissionDialog()
+        showNotification()
+        uploadNewAccessDocumentToFirebase(true)
+    }
+
+    private fun showNotification() {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        // Assign channel ID
+        val channelId = "notification_channel"
+
+        // Pass the intent to PendingIntent to start the
+        // next Activity
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        // Create a Builder object using NotificationCompat
+        // class. This will allow control over all the flags
+        val builder = NotificationCompat
+            .Builder(applicationContext, channelId)
+            .setAutoCancel(true)
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+            .setContentTitle("Data Access Request")
+            .setContentText("Hospital is requesting limited time access to your Personal History and Past Medical Records\"")
+            .setSmallIcon(R.drawable.stat_notify_more)
+
+        // Create an object of NotificationManager class to
+        // notify the
+        // user of events that happen in the background.
+        // Register the channel with the system
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        // Check if the Android Version is greater than Oreo
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId, "web_app",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+            Log.d(tag, "showNotification: version > O")
+        }
+        notificationManager.notify(0, builder.build())
     }
 
     private fun showInAppPermissionDialog() {
         Log.d(tag, "showInAppPermissionDialog() called")
+
         val dialogBuilder: AlertDialog.Builder =
-            AlertDialog.Builder(this, R.style.Theme_Material_Light_Dialog_Alert)
+            AlertDialog.Builder(applicationContext, R.style.Theme_Material_Light_Dialog_Alert)
 
         dialogBuilder.setTitle("Data Access Request")
         dialogBuilder.setMessage("Hospital ____ is requesting limited time access to your Personal History and Past Medical Records")
         dialogBuilder.setNegativeButton("Deny") {
                 dialog, _ ->
+
+            uploadNewAccessDocumentToFirebase(false)
             dialog.dismiss()
         }
         dialogBuilder.setPositiveButton("Allow") {
                 dialog, _ ->
 
-            uploadNewAccessDocumentToFirebase()
+            uploadNewAccessDocumentToFirebase(true)
             dialog.dismiss()
         }
 
-        val dialog: AlertDialog = dialogBuilder.create()
-        val dialogWindow: Window? = dialog.window
-        val dialogWindowAttributes: WindowManager.LayoutParams = dialogWindow!!.attributes
 
-        val layoutParams = WindowManager.LayoutParams()
-        layoutParams.copyFrom(dialogWindowAttributes)
-        layoutParams.width =
-            TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 350f, resources.displayMetrics)
-                .toInt()
-        layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT
-        dialogWindow.attributes = layoutParams
+        Handler(Looper.getMainLooper()).postDelayed({
 
-        dialogWindow.setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT)
-        dialogWindowAttributes.windowAnimations = R.style.Animation_Dialog
-        dialog.show()
+            val dialog: AlertDialog = dialogBuilder.create()
+            val dialogWindow: Window? = dialog.window
+            val dialogWindowAttributes: WindowManager.LayoutParams = dialogWindow!!.attributes
+
+            val layoutParams = WindowManager.LayoutParams()
+            layoutParams.copyFrom(dialogWindowAttributes)
+            layoutParams.width =
+                TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 350f, resources.displayMetrics)
+                    .toInt()
+            layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT
+            dialogWindow.attributes = layoutParams
+
+            dialogWindow.setType(WindowManager.LayoutParams.TYPE_APPLICATION_PANEL)
+            dialogWindowAttributes.windowAnimations = R.style.Animation_Dialog
+            dialog.show()
+
+        }, 1000L)
+
+
     }
 
-    private fun uploadNewAccessDocumentToFirebase() {
-        Log.d(tag, "RRG uploadNewAccessDocumentToFirebase() called")
+    private fun uploadNewAccessDocumentToFirebase(isAccessGiven: Boolean) {
+        Log.d(tag, "uploadNewAccessDocumentToFirebase() called with: isAccessGiven = $isAccessGiven")
 
-        val dataAccessRecord = hashMapOf(
-            IDENTIFIER_ACCESS_DATE to "Apr 07, 2022",
-            IDENTIFIER_FLAG_EXPIRED to false,
-            IDENTIFIER_FLAG_USER_PERMISSION to false,
-            IDENTIFIER_ACCESS_HOSPITAL_NAME to "Alberta Hospital"
-        )
+        // Data Preparation
+        val dataAccessRecord = if (isAccessGiven) {
+            hashMapOf(
+                IDENTIFIER_ACCESS_DATE to "Apr 07, 2022",
+                IDENTIFIER_FLAG_EXPIRED to false,
+                IDENTIFIER_FLAG_USER_PERMISSION to true,
+                IDENTIFIER_ACCESS_HOSPITAL_NAME to "Alberta Hospital"
+            )
+        } else {
+            hashMapOf(
+                IDENTIFIER_ACCESS_DATE to "Apr 07, 2022",
+                IDENTIFIER_FLAG_EXPIRED to false,
+                IDENTIFIER_FLAG_USER_PERMISSION to false,
+                IDENTIFIER_ACCESS_HOSPITAL_NAME to "Alberta Hospital"
+            )
+        }
 
+        // Upload
         mDatabaseInstance.collection(USER_COLLECTION_DATA_ACCESS)
             .document(LOGGED_IN_PATIENT_ID)
             .collection(USER_COLLECTION_RECORD)
